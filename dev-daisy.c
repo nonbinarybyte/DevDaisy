c17ec08ef8d78625518c0193cdf0fa160fb1f082
Kenny | 肯尼 <darleenfairy33@myyahoo.com>
2025-05-09 22:17:45 -0400

    BUILD 2 SRC

--- dev-daisy.c
+++ dev-daisy.c
@@ -11,20 +11,48 @@ typedef struct {
 } EditorTab;
 
 GtkWidget *notebook;
+gboolean dark_mode_enabled = FALSE;
+guint autosave_interval_id = 0;
 
-// Get current tab's EditorTab struct
 EditorTab* get_current_editor() {
     GtkWidget *tab_content = gtk_notebook_get_nth_page(GTK_NOTEBOOK(notebook),
                                                         gtk_notebook_get_current_page(GTK_NOTEBOOK(notebook)));
     return g_object_get_data(G_OBJECT(tab_content), "editor_tab");
 }
 
-void create_new_tab(const char *title, const char *content, const char *filepath, const char *lang_id) {
+void set_dark_mode(GtkSourceBuffer *buffer, gboolean enable_dark) {
+    GtkSourceStyleSchemeManager *ssm = gtk_source_style_scheme_manager_get_default();
+    GtkSourceStyleScheme *scheme = gtk_source_style_scheme_manager_get_scheme(
+        ssm, enable_dark ? "oblivion" : "classic");
+    gtk_source_buffer_set_style_scheme(buffer, scheme);
+}
+
+void autosave_current_tab() {
+    EditorTab *tab = get_current_editor();
+    if (!tab || !tab->filepath) return;
+
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tab->view));
+    GtkTextIter start, end;
+    gtk_text_buffer_get_bounds(buffer, &start, &end);
+    char *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+
+    g_file_set_contents(tab->filepath, text, -1, NULL);
+    g_free(text);
+}
+
+gboolean autosave_callback(gpointer data) {
+    autosave_current_tab();
+    return TRUE;
+}
+
+void create_new_tab(const char *title, const char *content, const char *filepath, const char *lang_id, gboolean dark_mode) {
     GtkSourceLanguageManager *lm = gtk_source_language_manager_get_default();
     GtkSourceLanguage *lang = lang_id ? gtk_source_language_manager_get_language(lm, lang_id) : NULL;
 
     GtkSourceBuffer *buffer = gtk_source_buffer_new_with_language(lang);
     gtk_text_buffer_set_text(GTK_TEXT_BUFFER(buffer), content ? content : "", -1);
+
+    if (dark_mode) set_dark_mode(buffer, TRUE);
 
     GtkWidget *view = gtk_source_view_new_with_buffer(buffer);
     gtk_source_view_set_show_line_numbers(GTK_SOURCE_VIEW(view), TRUE);
@@ -47,6 +75,24 @@ void create_new_tab(const char *title, const char *content, const char *filepath
     g_object_set_data(G_OBJECT(scroll), "editor_tab", tab);
 }
 
+const char* detect_language_from_extension(const char *filename) {
+    const char *ext = strrchr(filename, '.');
+    if (!ext) return NULL;
+
+    if (strcmp(ext, ".c") == 0) return "c";
+    else if (strcmp(ext, ".cpp") == 0) return "cpp";
+    else if (strcmp(ext, ".cs") == 0) return "c-sharp";
+    else if (strcmp(ext, ".java") == 0) return "java";
+    else if (strcmp(ext, ".go") == 0) return "go";
+    else if (strcmp(ext, ".asm") == 0 || strcmp(ext, ".s") == 0) return "nasm";
+    else if (strcmp(ext, ".html") == 0) return "html";
+    else if (strcmp(ext, ".css") == 0) return "css";
+    else if (strcmp(ext, ".js") == 0) return "javascript";
+    else if (strcmp(ext, ".rs") == 0) return "rust";
+
+    return NULL;
+}
+
 void open_file(GtkWidget *widget, gpointer user_data) {
     GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File", NULL,
         GTK_FILE_CHOOSER_ACTION_OPEN, "_Cancel", GTK_RESPONSE_CANCEL,
@@ -58,18 +104,8 @@ void open_file(GtkWidget *widget, gpointer user_data) {
         gsize length;
 
         if (g_file_get_contents(filename, &content, &length, NULL)) {
-            // Detect language
-            const char *ext = strrchr(filename, '.');
-            const char *lang_id = NULL;
-            if (ext) {
-                if (strcmp(ext, ".c") == 0) lang_id = "c";
-                else if (strcmp(ext, ".cpp") == 0) lang_id = "cpp";
-                else if (strcmp(ext, ".html") == 0) lang_id = "html";
-                else if (strcmp(ext, ".css") == 0) lang_id = "css";
-                else if (strcmp(ext, ".js") == 0) lang_id = "javascript";
-            }
-
-            create_new_tab(g_path_get_basename(filename), content, filename, lang_id);
+            const char *lang_id = detect_language_from_extension(filename);
+            create_new_tab(g_path_get_basename(filename), content, filename, lang_id, dark_mode_enabled);
             g_free(content);
         }
 
@@ -80,28 +116,7 @@ void open_file(GtkWidget *widget, gpointer user_data) {
 }
 
 void save_file(GtkWidget *widget, gpointer user_data) {
-    EditorTab *tab = get_current_editor();
-    if (!tab) return;
-
-    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tab->view));
-    GtkTextIter start, end;
-    gtk_text_buffer_get_bounds(buffer, &start, &end);
-    char *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
-
-    if (tab->filepath) {
-        g_file_set_contents(tab->filepath, text, -1, NULL);
-    } else {
-        GtkWidget *dialog = gtk_file_chooser_dialog_new("Save File", NULL,
-            GTK_FILE_CHOOSER_ACTION_SAVE, "_Cancel", GTK_RESPONSE_CANCEL,
-            "_Save", GTK_RESPONSE_ACCEPT, NULL);
-        if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-            tab->filepath = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-            g_file_set_contents(tab->filepath, text, -1, NULL);
-        }
-        gtk_widget_destroy(dialog);
-    }
-
-    g_free(text);
+    autosave_current_tab();
 }
 
 void build_and_run(GtkWidget *widget, gpointer user_data) {
@@ -115,11 +130,13 @@ void build_and_run(GtkWidget *widget, gpointer user_data) {
     if (!ext) return;
 
     char command[512];
-    char *output;
 
     if (strcmp(ext, ".c") == 0 || strcmp(ext, ".cpp") == 0) {
         snprintf(command, sizeof(command),
             "gcc \"%s\" -o /tmp/editor_run && /tmp/editor_run", tab->filepath);
+    } else if (strcmp(ext, ".rs") == 0) {
+        snprintf(command, sizeof(command),
+            "rustc \"%s\" -o /tmp/editor_run && /tmp/editor_run", tab->filepath);
     } else if (strcmp(ext, ".html") == 0 || strcmp(ext, ".js") == 0) {
         snprintf(command, sizeof(command), "xdg-open \"%s\"", tab->filepath);
     } else {
@@ -131,18 +148,32 @@ void build_and_run(GtkWidget *widget, gpointer user_data) {
     system(command);
 }
 
+void toggle_dark_mode(GtkCheckMenuItem *item, gpointer user_data) {
+    dark_mode_enabled = gtk_check_menu_item_get_active(item);
+
+    int n_pages = gtk_notebook_get_n_pages(GTK_NOTEBOOK(notebook));
+    for (int i = 0; i < n_pages; ++i) {
+        GtkWidget *tab_content = gtk_notebook_get_nth_page(GTK_NOTEBOOK(notebook), i);
+        EditorTab *tab = g_object_get_data(G_OBJECT(tab_content), "editor_tab");
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tab->view));
+        set_dark_mode(GTK_SOURCE_BUFFER(buffer), dark_mode_enabled);
+    }
+}
+
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Tabbed Code Editor");
+    gtk_window_set_title(GTK_WINDOW(window), "Dev Daisy");
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 700);
+    gtk_window_set_icon_from_file(GTK_WINDOW(window), "icon.png", NULL);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     GtkWidget *menu_bar = gtk_menu_bar_new();
+
     GtkWidget *file_menu = gtk_menu_new();
     GtkWidget *file_item = gtk_menu_item_new_with_label("File");
     GtkWidget *open_item = gtk_menu_item_new_with_label("Open");
@@ -153,7 +184,15 @@ int main(int argc, char *argv[]) {
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), save_item);
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), run_item);
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_item), file_menu);
+
+    GtkWidget *settings_menu = gtk_menu_new();
+    GtkWidget *settings_item = gtk_menu_item_new_with_label("Settings");
+    GtkWidget *dark_mode_item = gtk_check_menu_item_new_with_label("Dark Mode");
+    gtk_menu_shell_append(GTK_MENU_SHELL(settings_menu), dark_mode_item);
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(settings_item), settings_menu);
+
     gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), file_item);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), settings_item);
     gtk_box_pack_start(GTK_BOX(vbox), menu_bar, FALSE, FALSE, 0);
 
     notebook = gtk_notebook_new();
@@ -162,6 +201,9 @@ int main(int argc, char *argv[]) {
     g_signal_connect(open_item, "activate", G_CALLBACK(open_file), NULL);
     g_signal_connect(save_item, "activate", G_CALLBACK(save_file), NULL);
     g_signal_connect(run_item, "activate", G_CALLBACK(build_and_run), NULL);
+    g_signal_connect(dark_mode_item, "toggled", G_CALLBACK(toggle_dark_mode), NULL);
+
+    autosave_interval_id = g_timeout_add_seconds(60, autosave_callback, NULL);
 
     gtk_widget_show_all(window);
     gtk_main();
